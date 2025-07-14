41453568b5aba4f222ab464a5bdc4c4cdf1ceade
Fauzan Azima <fauzannazimaaa123@gmail.com>
2025-07-15 01:29:09 +0700

    koneksi database dan membuat chart

---
+++ db_koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$koneksi = new mysqli("localhost", "root", "", "bonus_evaluation_db");
+
+if ($koneksi->connect_error) {
+    die("Koneksi gagal: " . $koneksi->connect_error);
+}
+?>
---
+++ model/transaksi_model.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../db_koneksi.php';
+
+function getTotalPemasukan($koneksi) {
+    $query = $koneksi->query("SELECT SUM(pemasukan) AS total_pemasukan FROM transaksi");
+    if (!$query) {
+        die("Query total gagal: " . $koneksi->error);
+    }
+    $data = $query->fetch_assoc();
+    return $data['total_pemasukan'] ?? 0;
+}
+
+function getPemasukanBulanan($koneksi) {
+    $query = $koneksi->query("
+        SELECT MONTH(tanggal) AS bulan, 
+               SUM(pemasukan) AS total_bulanan 
+        FROM transaksi 
+        GROUP BY MONTH(tanggal)
+        ORDER BY MONTH(tanggal)
+    ");
+    if (!$query) {
+        die("Query bulanan gagal: " . $koneksi->error);
+    }
+
+    $data_bulanan = [];
+    while ($row = $query->fetch_assoc()) {
+        $data_bulanan[] = $row;
+    }
+    return $data_bulanan;
+}
+?>
